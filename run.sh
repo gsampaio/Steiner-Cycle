100ce7ffe35a9a2418277a51173998727d4f9d7b
Guilherme Martinez Sampaio <guilhermesampaio@gmail.com>
2011-11-26 05:29:56 -0200

    Typo error

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # run all the instances for 2 minutes
 make;
-for i in `ls instances`; do \
-    echo "Runing $i";
+for i in `ls instances/ `; do \
+    echo "Running $i";
     echo "$i:" >> out.out;
     ./RA073177.e instances/$i 120 >> out.out; 
     echo "\n" >>  out.out;\
